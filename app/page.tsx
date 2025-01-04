7d95dac387c5b1e4c2a0bb0cf173837e99424079
Damian Bulata <inamida16@gmail.com>
2025-01-04 14:26:56 +0200

    working auth dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { RegisterLink, LoginLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { requireUser } from '@/lib/requireUser';
+import { RegisterLink, LoginLink, LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return (
-    <div>
-      <LoginLink>Login</LoginLink>
-      <RegisterLink>Register</RegisterLink>
-    </div>
-  );
+export default async function Home() {
+  const user = await requireUser();
+
+  if (user) {
+    redirect('/dashboard');
+  } else {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <LoginLink>Login</LoginLink>
+        <RegisterLink>Register</RegisterLink>
+      </div>
+    );
+  }
 }
